c513669c9600a934586829e615120258afc825f5
iykecode93 <boscoiyke67@gmail.com>
2023-11-09 22:04:28 +0100

    Added event listener to buttons

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
 const specialChars = ["%", "*", "/", "-", "+", "="];
-let output = "";
+let output = " ";
 
 // Defined a function to calculate based on button clicked.
 const calculate = (btnvalue) => {
-    if (btnvalue === "=" && btnvalue !== "") {
-        // If output has "%", replace with "/100" before evaluating.
-        output = eval(output.replace("%",/100))
-    }
-    display.value = output
-}
+  if (btnvalue === "=" && output !== "") {
+    // If output has "%", replace with "/100" before evaluating.
+    output = eval(output.replace("%", "/100"));
+  } else if (btnvalue === "AC") {
+    output = " ";
+  } else if (btnvalue === "DEL") {
+    // If DEL button is clicked, remove the last character form the output
+    output = output.toString().slice(0, -1);
+  } else {
+    if (output === "" && specialChars.includes(btnvalue)) return;
+    output += btnvalue;
+  }
 
-// Add event listener to buttons, call calculate() on click.
+  display.value = output;
+};
+
+// Added event listener to buttons, call calculate() on click.
 buttons.forEach((button) => {
-    // Button click listener calls calculate() with dataset value as argument.
-    button.addEventListener("click", (e) => calculate(e.target.dataset.value))
-}) 
+  // Button click listener calls calculate() with dataset value as argument.
+  button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+});
